5dcb7bec03fb7346e79444822fc49c2f8bd416db
Nicolaas Weideman <nweidema@usc.edu>
2020-06-08 22:04:59 -0700

    Add docstrings

--- binalyzer/analyzers/analysis.py
+++ binalyzer/analyzers/analysis.py
@@ -3,9 +3,20 @@ from abc import ABC
 
 class Analysis(ABC):
 
-    '''
-    Should return something that implements AnalysisResults
-    '''
     @abc.abstractmethod
     def analyze(self, analysis_target):
+        '''Apply the analysis to analysis_target
+
+        Parameters
+        ----------
+        analysis_target : AnalysisTarget
+            An AnalysisTarget object containing the information of the target to analyze
+
+        Returns
+        -------
+        AnalysisResults
+            The results of the analysis stored in an AnalysisResults object
+
+
+        '''
         pass
--- binalyzer/target_discovery/analysis_target.py
+++ binalyzer/target_discovery/analysis_target.py
@@ -1,5 +1,20 @@
 class AnalysisTarget():
-    def __init__(self, full_file_path, file_name, file_size, file_md5):
+    '''
+    An object to store information of the target file to be analyzed.
+    '''
+    def __init__(self, full_file_path: str, file_name: str, file_size: int, file_md5: str):
+        '''
+        Parameters:
+        full_file_path : str
+            The absolute path of the file to analyze
+        file_name : str
+            The basename of the file to analyze
+        file_size : int
+            The size of the file to analyze, in bytes
+        file_md5 : str
+            The MD5 hash of the file to analyze
+
+        '''
         self.full_file_path = full_file_path
         self.file_name = file_name
         self.file_size = file_size
