b946c4915c5c85becbb26f693e9c33cf0b4a6133
Nicolaas <nweidema@usc.edu>
2020-06-18 10:00:42 -0700

    Update count_functions example to use the analyzer argument parser

--- examples/count_functions/driver.py
+++ examples/count_functions/driver.py
@@ -7,24 +7,25 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from binalyzer.analyzers.sequential_analyzer import SequentialAnalyzer
 from binalyzer.analyzers.parallel_analyzer import ParallelAnalyzer
+from binalyzer.util.analyzer_argument_parser import ParallelAnalyzerArgumentParser
 
 from count_functions_analysis import CountFunctionsAnalysis
 
 def main():
-    parser = argparse.ArgumentParser('Count the number of functions in binaries.')
-    parser.add_argument('--root_dir', required=True, help='the root directory from where to search for all the elf files')
-    parser.add_argument('--nthreads', default=1, type=int, help='number of processes to use.')
+    analyzer_argument_parser = ParallelAnalyzerArgumentParser()
+    parser = argparse.ArgumentParser('Count the number of functions in binaries.', parents=[analyzer_argument_parser])
 
     args = parser.parse_args()
     root_dir = args.root_dir
+    elf_list = args.elf_list
     nthreads = args.nthreads
     
     analysis = CountFunctionsAnalysis()
     print("Sequential analysis:")
-    seq_analyzer = SequentialAnalyzer(analysis, root_dir=root_dir, results_path='./example_sequential_analysis_results')
+    seq_analyzer = SequentialAnalyzer(analysis, root_dir=root_dir, elf_list=elf_list, results_path='./example_sequential_analysis_results')
     seq_analyzer.run_analysis()
     print("Parallel analysis:")
-    par_analyzer = ParallelAnalyzer(analysis, root_dir=root_dir, nthreads=nthreads, results_path='./example_parallel_analysis_results')
+    par_analyzer = ParallelAnalyzer(analysis, root_dir=root_dir, elf_list=elf_list, nthreads=nthreads, results_path='./example_parallel_analysis_results')
     par_analyzer.run_analysis()
 
 if __name__ == "__main__":
