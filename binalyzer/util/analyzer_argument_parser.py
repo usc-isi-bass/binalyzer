ae1c57495e1a47a7ba03f8478b5cf507ecf54961
Nicolaas <nweidema@usc.edu>
2020-06-26 16:39:21 -0700

    Add arguments for the binalyzer argument parser in an argument group

--- binalyzer/util/analyzer_argument_parser.py
+++ binalyzer/util/analyzer_argument_parser.py
@@ -9,12 +9,13 @@ class AnalyzerArgumentParser(argparse.ArgumentParser):
 
     def __init__(self, *args, **kwargs):
         argparse.ArgumentParser.__init__(self, *args, add_help=False, **kwargs)
-        self.add_argument('--root_dir', help='The root directory from where the search for analysis targets will start. (mutually exclusive with elf_list)')
-        self.add_argument('--elf_list', help='A list of strings specifying the file names of the analysis targets. (mutually exclusive with root_dir)')
-        self.add_argument('--break_limit', help='An upper bound on the number of AnalysisTargets to generate', type=int)
-        self.add_argument('--remove_duplicates', help='Ensure all AnalysisTargets have a unique MD5 hash', type=bool, default=True)
-        self.add_argument('--results_path', help='Either a directory or a filename. If a directory, the analysis results will be stored in a file in this directory (the file name will be the time the analysis started). If a filename, the analysis results will be stored in this file. The default is the current working directory.', default=os.getcwd())
-        self.add_argument('--timeout', help='The number of seconds after which the analysis of a single AnalysisTarget will be stopped.', type=int, default=None)
+        binalyzer_arg_group = self.add_argument_group(title='Binalyzer Arguments', description='The arguments for the binalyzer driver')
+        binalyzer_arg_group.add_argument('--root_dir', help='The root directory from where the search for analysis targets will start. (mutually exclusive with elf_list)')
+        binalyzer_arg_group.add_argument('--elf_list', help='A list of strings specifying the file names of the analysis targets. (mutually exclusive with root_dir)')
+        binalyzer_arg_group.add_argument('--break_limit', help='An upper bound on the number of AnalysisTargets to generate', type=int)
+        binalyzer_arg_group.add_argument('--remove_duplicates', help='Ensure all AnalysisTargets have a unique MD5 hash', type=bool, default=True)
+        binalyzer_arg_group.add_argument('--results_path', help='Either a directory or a filename. If a directory, the analysis results will be stored in a file in this directory (the file name will be the time the analysis started). If a filename, the analysis results will be stored in this file. The default is the current working directory.', default=os.getcwd())
+        binalyzer_arg_group.add_argument('--timeout', help='The number of seconds after which the analysis of a single AnalysisTarget will be stopped.', type=int, default=None)
 
 class SequentialAnalyzerArgumentParser(AnalyzerArgumentParser):
     '''
@@ -29,5 +30,6 @@ class ParallelAnalyzerArgumentParser(AnalyzerArgumentParser):
     '''
     def __init__(self, *args, **kwargs):
         AnalyzerArgumentParser.__init__(self, *args, **kwargs)
-        self.add_argument('--nthreads', required=False, default=1, type=int, help='The number of processes to use.')
+        parallel_arg_group = self.add_argument_group(title='Parallel Binalyzer Arguments', description='The arguments for the parallel binalyzer driver')
+        parallel_arg_group.add_argument('--nthreads', required=False, default=1, type=int, help='The number of processes to use.')
 
