3d6cc80575ad60c0daf7a31dead0faab7de2a1d6
Nicolaas Weideman <nweidema@usc.edu>
2020-08-04 07:07:02 -0700

    Update examples to follow new api

--- examples/count_functions/count_functions_analysis.py
+++ examples/count_functions/count_functions_analysis.py
@@ -12,25 +12,38 @@ from binalyzer.analyzers.analysis import Analysis
 from binalyzer.analyzers.analysis_results import AnalysisResults
 
 class CountFunctionsAnalysis(Analysis):
+
+    def results_constructor(self):
+        return CountFunctionsAnalysisResults
     
-    def analyze(self, analysis_target):
+    def analyze(self, analysis_target, analysis_results):
         num_funcs = None
         errs = []
         try:
             full_target_file_path = analysis_target.full_file_path
             proj = angr.Project(full_target_file_path, auto_load_libs=False)
-            cfg = proj.analyses.CFGFast()
-            num_funcs = len(cfg.functions.items())
+            cfg = proj.analyses.CFGFast(normalize=True)
+            funcs = cfg.functions.items()
+            for func_addr, func in funcs:
+                print("func: {}@0x{:x}".format(func.name, func_addr))
+            num_funcs = len(funcs)
+            analysis_results.set_num_funcs(num_funcs)
         except Exception as e:
             errs = [str(e)]
+            analysis_results.errs = errs
 
-        return CountFunctionsAnalysisResults(num_funcs, errs)
+        #return CountFunctionsAnalysisResults(num_funcs, errs)
 
 class CountFunctionsAnalysisResults(AnalysisResults):
-    def __init__(self, num_funcs, errs):
+    def __init__(self):
         AnalysisResults.__init__(self)
+        self.num_funcs = None
+        self.errs = []
+
+    def get_num_funcs(self):
+        return self.num_funcs
+    def set_num_funcs(self, num_funcs):
         self.num_funcs = num_funcs
-        self.errs = errs
 
     def get_tracked_events(self):
         tracked_events = {}
