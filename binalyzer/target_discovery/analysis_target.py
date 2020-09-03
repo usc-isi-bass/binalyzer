8e2bac593328800ab0716b6945c5245c09cf7243
Nicolaas Weideman <nweidema@usc.edu>
2020-09-03 06:18:03 -0700

    Add hash and equals methods to analysis target for hashing

--- binalyzer/target_discovery/analysis_target.py
+++ binalyzer/target_discovery/analysis_target.py
@@ -23,4 +23,14 @@ class AnalysisTarget():
 
     def __str__(self):
         return "<AnalysisTarget {} ({} bytes) MD5={}>".format(self.full_file_path, self.file_size, self.file_md5)
+
+    def __eq__(self, o):
+        if o is None:
+            return False
+        if not isinstance(o, AnalysisTarget):
+            return False
+        return self.full_file_path == o.full_file_path and self.file_name == o.file_name and self.file_size == o.file_size and self.file_md5 == o.file_md5
+
+    def __hash__(self):
+        return hash("{}{}{}{}".format(self.full_file_path, self.file_name, self.file_size, self.file_md5))
         
