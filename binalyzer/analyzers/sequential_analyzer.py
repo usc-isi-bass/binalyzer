e9b7c73e7e81d3f580e32fbd02ab21c4e24f0c1c
Nicolaas Weideman <nweidema@usc.edu>
2020-09-24 18:09:17 -0700

    Fix sequential analyzer to work with multiprocessing (for timeouts)

--- binalyzer/analyzers/sequential_analyzer.py
+++ binalyzer/analyzers/sequential_analyzer.py
@@ -1,13 +1,17 @@
 import abc
 from abc import ABC
+import multiprocessing
+import multiprocessing.managers
 
 from binalyzer.analyzers.analyzer import Analyzer
 
 class SequentialAnalyzer(Analyzer):
 
     def analyze_targets(self, analysis_targets):
-        for analysis_target in analysis_targets:
-            analysis_results, analysis_results = self.analyze_target(analysis_target)
-            yield analysis_target, analysis_results
+        with multiprocessing.managers.BaseManager() as manager:
+            analysis_results_objects = [manager.AnalysisResults() for i in range(len(analysis_targets))]
+            args = zip(analysis_targets, analysis_results_objects)
+            for analysis_target, analysis_result in map(self.analyze_target, args):
+                    yield analysis_target, analysis_result
 
 
