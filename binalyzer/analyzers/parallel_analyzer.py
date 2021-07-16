7726753d75fd72e33e4ce664c04482164a970a7b
Nicolaas Weideman <nweidema@usc.edu>
2021-07-16 12:17:22 -0700

    Update parallel analyzer to create a pool of processes instead of threads

--- binalyzer/analyzers/parallel_analyzer.py
+++ binalyzer/analyzers/parallel_analyzer.py
@@ -1,10 +1,10 @@
 from abc import ABC
+import signal
 
 
 import multiprocessing
 import multiprocessing.managers
 import concurrent
-import multiprocessing.dummy as threading
 
 from binalyzer.analyzers.analyzer import Analyzer
 from binalyzer.analyzers.analysis_results import ErrorAnalysisResults
@@ -24,7 +24,9 @@ class ParallelAnalyzer(Analyzer):
         with multiprocessing.managers.BaseManager() as manager:
             analysis_results_objects = [manager.AnalysisResults() for i in range(len(analysis_targets))]
             args = zip(analysis_targets, analysis_results_objects)
-            with threading.Pool(self._nthreads) as pool:
+            if self._timeout is not None:
+                signal.signal(signal.SIGALRM,  signal.SIG_IGN)
+            with multiprocessing.Pool(self._nthreads) as pool:
                 for analysis_target, analysis_result in pool.imap_unordered(self.analyze_target, args):
                         yield analysis_target, analysis_result
 
